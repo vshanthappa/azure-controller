2936997c741c48e32e71f908e06bf662ae307b62
Vijay Shanthappa <88101286+vshanthappa@users.noreply.github.com>
2023-02-07 09:21:33 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
   features {}
+
+  subscription_id   = var.az-sub-id
+  tenant_id         = var.az-sub-ten-id
+  client_id         = var.az-sub-cli-id
+  client_secret     = var.az-sub-cli-sec
 }

fffec6472362c5671d7f04d1a3ac49ea25ca30ba
Vijay Shanthappa <88101286+vshanthappa@users.noreply.github.com>
2023-02-07 10:10:35 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,1 @@
-variable "az-sub-id" {
-  type        = string
-}
 
-variable "az-sub-ten-id" {
-  type        = string
-}
-
-variable "az-sub-cli-id" {
-  type        = string
-}
-
-variable "az-sub-cli-sec" {
-  type        = string
-}

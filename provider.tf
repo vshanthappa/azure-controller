49296967800aff43aeb733e7de43eebc04a8c1de
Vijay Shanthappa <88101286+vshanthappa@users.noreply.github.com>
2023-02-07 09:44:49 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.30.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 1.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 

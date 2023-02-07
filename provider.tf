99f0d42cf0218d79ba5b05cb7ae08ef333ee5b46
Vijay Shanthappa <88101286+vshanthappa@users.noreply.github.com>
2023-02-07 09:51:27 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -22,10 +22,14 @@ terraform {
 }
 
 provider "azuread" {
-#  features {}
 
-  subscription_id   = var.az-sub-id
+
   tenant_id         = var.az-sub-ten-id
   client_id         = var.az-sub-cli-id
   client_secret     = var.az-sub-cli-sec
+}
+
+provider "azurerm" {
+  features {}
+   subscription_id   = var.az-sub-id
 }
